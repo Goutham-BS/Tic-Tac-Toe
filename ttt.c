bcfe12097e8c9f8784bbe2bafb2c1ff6f7fe1d47
Goutham <167563063+Goutham-BS@users.noreply.github.com>
2025-02-12 17:36:30 +0530

    Create ttt.c

---
+++ ttt.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+//To aletrnate between X and O
+char turn(int c) {
+    if (c % 2 == 0) {
+        return 'X';
+    } else {
+        return 'O';
+    }
+}
+
+void board(char plots[]) {
+    printf("|%c|%c|%c|\n|%c|%c|%c|\n|%c|%c|%c|\n", 
+            plots[0], plots[1], plots[2], 
+            plots[3], plots[4], plots[5], 
+            plots[6], plots[7], plots[8]);
+}
+
+int main() {
+    char plots[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
+    int choice, count = 0;
+    
+    while (1) {
+        board(plots);
+        printf("Player %c, enter a choice: ", turn(count));
+        scanf("%d", &choice);
+
+        if (choice < 0 || choice > 8) {
+            printf("Invalid move! Try again.\n");
+            continue;
+        }
+        plots[choice] = turn(count);
+        count++;
+    }  
+    return 0;
+}
